8c4c2a90117b2622e8e48730b460468cb8834d8b
@maptaul <maptaulislam1@gmail.com>
2025-01-06 19:52:34 +0600

    lottie

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import toast from "react-hot-toast";
+import Lottie from "react-lottie";
+import newsletterAnimation from "../assets/Lottie/newsletter.json";
 
 const Newsletter = () => {
   const [email, setEmail] = useState("");
@@ -15,8 +17,21 @@ const Newsletter = () => {
     setEmail("");
   };
 
+  // Lottie configuration
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: newsletterAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
-    <div className=" rounded-md  mx-auto text-white py-12 px-6">
+    <div className="rounded-md bg-[#2b3a49] mx-auto md:flex items-center text-white py-6 px-6">
+      <div className="text-center lg:text-left w-96 mx-auto">
+        <Lottie options={defaultOptions} height={400} width={400} />
+      </div>
       <div className="max-w-4xl mx-auto text-center">
         <h2 className="text-3xl md:text-4xl font-bold mb-4">
           Subscribe to Chill Gamer
